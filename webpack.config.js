1e94cd49d417e7e4c01166c61e7b3b73119ac9a1
Zach Cyrus <zscyrus31@gmail.com>
2021-08-26 16:32:53 -0400

    Installed copy webpack and configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     entry: './src/scripts/script.js',
@@ -11,7 +12,27 @@ module.exports = {
             {
                 test: /\.scss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                        name: '[name].[ext]',
+                        outputPath: '/images/',
+                        pubicPath: '/images/'
+                      }
+                    }
+                  ],
+            },
         ],
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/images", to: "dist/images"}
+            ]
+        })
+    ]
 };
